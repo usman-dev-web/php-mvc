34725068d094fc508ab382b9f235cc768061d5f2
M. Usman Maulana <um59411@gmail.com>
2022-12-11 09:18:04 +0700

    menambah file about dan home

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About
+{
+    public function index($nama = "Usman", $pekerjaan = "Web dev")
+    {
+        echo "Hallo, nama saya $nama, saya seorang $pekerjaan";
+    }
+    public function page()
+    {
+        echo "About/page";
+    }
+}
