bea8125d6b0da8e6f60f60177ddaeb65750075f9
M. Usman Maulana <um59411@gmail.com>
2022-12-11 21:16:20 +0700

    menambah baris untuk menampilkan flash alert

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,8 +1,13 @@
 <div class="container" mt-3>
 
+    <div class="row">
+        <div class="col-lg-6">
+            <?php Flasher::flash() ?>
+        </div>
+    </div>
 
     <div class="row">
-        <div class="col-6">
+        <div class="col-lg-6">
             <!-- Button trigger modal -->
             <button type="button" class="btn btn-primary mt-4" data-toggle="modal" data-target="#formModal">
                 Tambah Data Mahasiswa
