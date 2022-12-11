bea85b3f2017698eab0c44a0b8cbb86ecfcdfc37
M. Usman Maulana <um59411@gmail.com>
2022-12-11 21:15:04 +0700

    mengubah isi tag p

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,7 +3,7 @@
         <h1 class="display-4">Selamat Datang</h1>
         <p class="lead"><?= $data["nama"]; ?>.</p>
         <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p>Dream->Commitment->Consistent = SUCCESS.</p>
         <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </div>
 </div>
