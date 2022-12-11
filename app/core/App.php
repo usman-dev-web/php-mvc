1856dbc78e5e9d8fc55d682b72636f992adbbe99
M. Usman Maulana <um59411@gmail.com>
2022-12-11 11:09:00 +0700

    fix error access array offset on value type null

--- app/core/App.php
+++ app/core/App.php
@@ -1,6 +1,7 @@
 <?php
 
-class App{
+class App
+{
     protected $controller = "Home";
     protected $method = "index";
     protected $params = [];
@@ -8,9 +9,12 @@ class App{
     public function __construct()
     {
         $url = $this->parseUrl();
-        
-        //controller
-        if(file_exists("../app/controllers/" . $url[0] . ".php")){
+
+        // controller
+        if ($url == NULL) {
+            $url = [$this->controller];
+        }
+        if (file_exists("../app/controllers/" . $url[0] . ".php")) {
             $this->controller = $url[0];
             unset($url[0]);
         }
@@ -19,27 +23,29 @@ class App{
         $this->controller = new $this->controller;
 
         // method
-        if(isset($url[1])){
-            if(method_exists($this->controller, $url[1])){
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
                 $this->method = $url[1];
+                // var_dump($url);
                 unset($url[1]);
             }
         }
 
         //params
-        if(!empty($url)){
-           $this->params = array_values($url);
+        if (!empty($url)) {
+            $this->params = array_values($url);
         }
 
         // jalankan controller dan method serta kirimkan params jika ada
-        call_user_func_array([$this->controller, $this->method],$this->params);
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
-    public function parseUrl(){
+    public function parseUrl()
+    {
         // routing url
-        if(isset($_GET['url'])){
+        if (isset($_GET['url'])) {
             // menghapus tanda \ pada akhir url
-            $url = rtrim($_GET['url'],"/");
+            $url = rtrim($_GET['url'], "/");
             // menambah filter untuk membersihkan url dari karakter yang aneh
             $url = filter_var($url, FILTER_SANITIZE_URL);
             // memecah string url
