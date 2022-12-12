68251f20204564643504d748afcf5da5e18de231
M. Usman Maulana <um59411@gmail.com>
2022-12-12 20:09:29 +0700

    menambah fungsi untuk menghapus data

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -28,4 +28,16 @@ class Mahasiswa extends Controller{
             exit;
         }
     }
+
+    public function hapus($id){
+        if($this->model("Mahasiswa_model")->hapusDataMahasiswa($id) > 0 ){
+            Flasher::setFlash("Berhasil", "dihapus", "success");
+            header("Location: " . BASEURL . "/mahasiswa");
+            exit;
+        }else{
+            Flasher::setFlash("Gagal", "dihapus", "danger");
+            header("Location: " . BASEURL . "/mahasiswa");
+            exit;
+        }
+    }
 }
